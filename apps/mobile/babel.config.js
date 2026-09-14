7b2200b8634ea08a9b5329a846e9cac14ee482a7
Tribe-Connect <aaron@tribeconnect.io>
2026-09-14 11:02:33 +0100

    Add files via upload

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,7 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // Reanimated 4 moved worklets into their own package; the plugin MUST be last.
-    plugins: ['react-native-worklets/plugin'],
   };
 };
